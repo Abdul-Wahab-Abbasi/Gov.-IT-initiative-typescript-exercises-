157015a2fe7feca4a9c545d9f6a6ea136583e200
Abdul Wahab Abbasi <wahababbasi923@gmail.com>
2024-02-17 20:46:03 +0500

    Assignment1(PersonalMessage)

---
+++ index.js
@@ -0,0 +1,3 @@
+var person = "Wahab";
+var message = "Hello ".concat(person, ",\n would you like to learn python today?");
+console.log(message);
---
+++ index.ts
@@ -0,0 +1,3 @@
+const person:string="Wahab";
+const message:string=`Hello ${person},\n would you like to learn python today?`;
+console.log(message);
