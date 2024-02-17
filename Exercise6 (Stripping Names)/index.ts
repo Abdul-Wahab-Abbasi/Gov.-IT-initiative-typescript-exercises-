e8f9a285051ff6ebef80ad8c7bf966cf00e3d52b
Abdul Wahab Abbasi <149320044+Abdul-Wahab-Abbasi@users.noreply.github.com>
2024-02-17 23:03:47 +0500

    comment added

--- Exercise6 (Stripping Names)/index.ts
+++ Exercise6 (Stripping Names)/index.ts
@@ -1,3 +1,6 @@
+// Date: 2/17/24
+// Stripping Names
+// Program by Abdul Wahab
 const person:string="\t\tAbdul Wahab\n";
 console.log(`Name of Person with spaces: ${person}`);
 console.log(`Name of Person without spaces: ${person.trim()}`);
