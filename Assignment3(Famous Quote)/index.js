ec612eac016552bc12fef534e0b899e273199377
Abdul Wahab Abbasi <149320044+Abdul-Wahab-Abbasi@users.noreply.github.com>
2024-02-17 21:44:09 +0500

    assignment 3 completed

---
+++ Assignment3(Famous Quote)/index.js
@@ -0,0 +1,3 @@
+var author = "Jack Ma";
+var quote = "If we are a good team and know what we want to do, one of us can defeat ten of them.";
+console.log("".concat(author, " once said, \"").concat(quote, "\""));
---
+++ Assignment3(Famous Quote)/index.ts
@@ -0,0 +1,4 @@
+const author:string="Jack Ma";
+const quote:string="If we are a good team and know what we want to do, one of us can defeat ten of them."
+
+console.log(`${author} once said, "${quote}"`);
