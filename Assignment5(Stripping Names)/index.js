894415dad1bdf1521e5cca1a11039316e47b1750
Abdul Wahab Abbasi <149320044+Abdul-Wahab-Abbasi@users.noreply.github.com>
2024-02-17 21:57:31 +0500

    assignment 5 completed

---
+++ Assignment5(Stripping Names)/index.js
@@ -0,0 +1,3 @@
+var person = "\t\tAbdul Wahab\n";
+console.log("Name of Person with spaces: ".concat(person));
+console.log("Name of Person without spaces: ".concat(person.trim()));
---
+++ Assignment5(Stripping Names)/index.ts
@@ -0,0 +1,3 @@
+const person:string="\t\tAbdul Wahab\n";
+console.log(`Name of Person with spaces: ${person}`);
+console.log(`Name of Person without spaces: ${person.trim()}`);
